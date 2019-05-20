949b2c714a6320c5ea74d7c1e18a2a7f40ec7405
Platekun <carloscamilo.loboulloque@gmail.com>
2019-05-19 21:28:15 -0500

    Migrate to stopwatch to state machine hooks

--- src/stopwatch/Stopwatch.js
+++ src/stopwatch/Stopwatch.js
@@ -1,49 +1,32 @@
 import React from "react";
-import * as XState from "xstate";
+import { useMachine } from "@xstate/react";
 
 import { Screen, AppBar } from "../components";
 import { StopwatchTime } from "./StopwatchTime";
 import { StopwatchControls } from "./StopwatchControls";
 import { stopwatchMachine } from "./stopwatch-machine";
 
-export class Stopwatch extends React.Component {
-  state = {
-    current: stopwatchMachine.initialState
-  };
+export function Stopwatch() {
+  const [current, send] = useMachine(stopwatchMachine);
 
-  service = XState.interpret(stopwatchMachine).onTransition(current =>
-    this.setState({ current })
+  const { laps, time } = current.context;
+
+  return (
+    <Screen>
+      <AppBar>Stopwatch</AppBar>
+
+      <StopwatchTime time={time} />
+
+      <StopwatchControls
+        current={current}
+        onStart={() => send("START")}
+        onCancel={() => send("CANCEL")}
+        onPause={() => send("PAUSE")}
+        onResume={() => send("RESUME")}
+        onLap={() => send("LAP")}
+        laps={laps}
+        time={time}
+      />
+    </Screen>
   );
-
-  componentDidMount() {
-    this.service.start();
-  }
-
-  componentWillUnmount() {
-    this.service.stop();
-  }
-
-  render() {
-    const { send } = this.service;
-    const { current } = this.state;
-
-    return (
-      <Screen>
-        <AppBar>Stopwatch</AppBar>
-
-        <StopwatchTime time={current.context.time} />
-
-        <StopwatchControls
-          current={current}
-          onStart={() => send("START")}
-          onCancel={() => send("CANCEL")}
-          onPause={() => send("PAUSE")}
-          onResume={() => send("RESUME")}
-          onLap={() => send("LAP")}
-          laps={current.context.laps}
-          time={current.context.time}
-        />
-      </Screen>
-    );
-  }
 }
